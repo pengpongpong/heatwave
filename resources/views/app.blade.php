c2e430c61fb30a6bb569a0c2fc2d7d357cc462d0
Phuong T <t.m.phuong@hotmail.com>
2024-01-09 20:22:44 +0100

    update vite config in app.blade

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,7 +9,7 @@
     <!-- Scripts -->
     @routes
     @viteReactRefresh
-    @vite(['resources/js/app.tsx', "resources/js/Pages/{$page['component']}.tsx"])
+    @vite(['resources/js/app.tsx'])
     @inertiaHead
 </head>
 
